7b78312905cd9637f4a0e488cb3e48a356c11987
Stefan Kopieczek <stefankopieczek@gmail.com>
2014-03-09 09:39:24 +0000

    Added docstrings and better commenting to fd.py

--- fd/fd.py
+++ fd/fd.py
@@ -4,6 +4,16 @@ import sys
 
 
 def finddirs(dirname, startdir, prefix=True):
+    """Determines the depth in the folder tree based at startdir at which some
+       folders first match the dirname parameter, and the returns all such
+       folders in that level.
+
+       So in a structure <a, a/b, a/c, a/c/b>, finddirs('b', 'a') would return
+       [`a/b'] only.
+
+       If prefix is True, match any folders which start with dirname.
+       Otherwise, match any folders whose name is precisely dirname.
+    """
     is_match = dirname.__eq__
     if prefix:
         def is_match(directory):
@@ -14,12 +24,14 @@ def finddirs(dirname, startdir, prefix=True):
     while True:
         dirs_on_next_level = []
         matches = set([])
+
         for directory in dirs_on_current_level:
             try:
                 contents = [os.path.join(directory, f)
                             for f in os.listdir(directory)]
             except OSError:
-                pass
+                continue  # Don't complain about permissions etc.
+
             folders = [item for item in contents if os.path.isdir(item)]
             for folder in folders:
                 if is_match(os.path.basename(folder)):
@@ -33,11 +45,13 @@ def finddirs(dirname, startdir, prefix=True):
         else:
             dirs_on_current_level = dirs_on_next_level
 
+    # No matches found.
     return []
 
 
 def exit(exit_code):
-    """Workaround for http://bugs.python.org/issue11380."""
+    """Workaround for http://bugs.python.org/issue11380.
+       Exits and returns an error code, but isn't confused by pipes."""
     try:
         sys.stdout.close()
     except:
@@ -54,6 +68,8 @@ if __name__ == "__main__":
         sys.stderr.write("Usage: python fd.py <directory> [start-height]\n")
         exit(1)
 
+    # The starting directory is the nth parent of the current working
+    # directory, where n is the height.
     startdir = os.getcwd()
     height = int(sys.argv[2]) if len(sys.argv) > 2 else 0
     while height > 0:
@@ -63,17 +79,18 @@ if __name__ == "__main__":
     matches = finddirs(sys.argv[1], startdir, prefix=True)
 
     if len(matches) == 0:
-        # No match was found, so the command should CD to the current
-        # directory - i.e. do nothing.
+        # No match was found.
+        # Output the current directory for cd, and show an error.
         sys.stderr.write("No match found.\n")
         sys.stdout.write('.')
         exit(2)
     elif len(matches) > 1:
         # Multiple matches found.
-        # CD to the current directory and show an error.
+        # Output the current directory for cd,  and show an error.
         sys.stderr.write("Ambiguous command: " + ', '.join(matches) + "\n")
         sys.stdout.write('.')
         exit(3)
     else:
+        # Unique match found. Stdout it so that cd can pick it up.
         sys.stdout.write(list(matches)[0])
         exit(0)
